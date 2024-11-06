ded547dc01a6e5f2cb54caf6c35d63273d896384
magusvagus <magusvagus@proton.me>
2024-11-06 18:49:20 +0100

    added threshold, for object detection

--- main.py
+++ main.py
@@ -8,22 +8,24 @@ import numpy as np
 # NOTE: image data of farmables and bullets have been retaken and are now the same depth, no conversion needed!
 # FIX: same has still to be done for > enemies, player, UI.
 screenshot_img = cv.imread('./screenshots_diepio/gameplay_screenshots/Oct30_224501.png', cv.IMREAD_UNCHANGED)
-farm_yellow_cube = cv.imread('./screenshots_diepio/farming/farm_cube2.png', cv.IMREAD_UNCHANGED)
+yellow_cube = cv.imread('./screenshots_diepio/farming/farm_cube2.png', cv.IMREAD_UNCHANGED)
 
-result = cv.matchTemplate(screenshot_img, farm_yellow_cube, cv.TM_CCOEFF_NORMED)
+result = cv.matchTemplate(screenshot_img, yellow_cube, cv.TM_CCOEFF_NORMED)
 
 # max value location
 minVal, maxVal, minLoc, maxLoc = cv.minMaxLoc(result)
-print(maxLoc)
-
-#thresh = cv.threshold(result, 0.7, 1.0, cv.THRESH_BINARY)
-#print(thresh)
 
 # fetch image size of the needle
-farm_x, farm_y, _ = farm_yellow_cube.shape
+y_Cube, x_Cube, _ = yellow_cube.shape
 
 # draw rectangle around object
-cv.rectangle(screenshot_img, maxLoc, (maxLoc[0] + farm_x, maxLoc[1] + farm_y), (0,255,0), 2, 8, 0)
+
+# pentagon needs highier threshold, player character gives false positive.
+threshold = 0.45
+yLoc, xLoc = np.where(result >= threshold)
+
+for i, j in zip(yLoc, xLoc):
+    cv.rectangle(screenshot_img, (j, i), (j + y_Cube, i + x_Cube), (0,255,0), 2)
 
 cv.imshow('Result', screenshot_img)
 cv.waitKey()
