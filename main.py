f2dcf43b39a7499e505f71aa4fdbe52a6248def9
magusvagus <magusvagus@proton.me>
2024-11-11 20:04:34 +0100

    put window capturing code in seperate file 'window_capture.py' to organize overall code.

--- main.py
+++ main.py
@@ -1,68 +1,6 @@
 import cv2 as cv
 import numpy as np
-from threading import Thread, Lock
-import Xlib
-import Xlib.display
-from Xlib import X
-
-# NOTE: try not to use OOP, lets dumb it down to functions. XCB might be a more wise option for the future (lower level -> faster).
-class WindowCapture:
-    stopped = True
-    lock = None
-    screenshot = None
-    windowId = None
-
-    def __init__(self, window_name):
-        self.lock = Lock()
-        self.screenshot = None 
-        display = Xlib.display.Display()
-        try:
-            root = display.screen().root
-            windowIDs = root.get_full_property(display.intern_atom('_NET_CLIENT_LIST'), X.AnyPropertyType).value
-
-            for windowID in windowIDs:
-                window = display.create_resource_object('window', windowID)
-                window_title_property = window.get_full_property(display.intern_atom('_NET_WM_NAME'), 0)
-
-                if window_title_property and window_name.lower() in window_title_property.value.decode('utf-8').lower():
-                    self.windowId = windowID
-
-            if not self.windowId:
-                raise Exception('Window not found: {}'.format(window_name))
-        finally:
-            display.close()
-
-    def get_screenshot(self):
-        display = Xlib.display.Display()
-        window = display.create_resource_object('window', self.windowId)
-
-        geometry = window.get_geometry()
-        width, height = geometry.width, geometry.height
-
-        pixmap = window.get_image(0, 0, width, height, X.ZPixmap, 0xffffffff)
-        data = pixmap.data
-        image = np.frombuffer(data, dtype='uint8').reshape((height, width, 4))
-        display.close()
-        return image
-
-    # threading methods
-    def start(self):
-        self.stopped = False
-        t = Thread(target=self.run)
-        t.start()
-
-    def stop(self):
-        self.stopped = True
-
-    def run(self):
-        while not self.stopped:
-            # get an updated image of the game
-            screenshot = self.get_screenshot()
-            # lock the thread while updating the results
-            self.lock.acquire()
-            self.screenshot = screenshot
-            self.lock.release()
-
+from window_capture import WindowCapture
 
 
 windowCapture = WindowCapture('librewolf')
@@ -73,13 +11,14 @@ run = True
 # FIX: UI image data has to be retaken.
 yellow_cube = cv.imread('./screenshots_diepio/farming/farm_cube2.png', cv.IMREAD_UNCHANGED)
 
+
 # main loop
 while run:
     if windowCapture.screenshot is not None:
         image = windowCapture.screenshot
 
         # image variable needs conversion, otherwise cv.rectangle function throws err.
-        # NOTE: need workaround
+        # NOTE: need workaround and color are off.
         image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 
         result = cv.matchTemplate(image, yellow_cube, cv.TM_CCOEFF_NORMED)
