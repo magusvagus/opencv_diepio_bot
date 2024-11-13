26e653dd25bcc76b564ef5af69fc299b780241c1
magusvagus <magusvagus@proton.me>
2024-11-13 19:31:22 +0100

    put HSV thresholding functions from main.py into seperate file -> hsv_thresholding.py

---
+++ hsv_thresholding.py
@@ -0,0 +1,55 @@
+import cv2 as cv
+import numpy as np
+
+
+# create trackbars
+def TrackbarsHSV(windowName, LH, LS, LV, UH, US, UV, steps, maxVal):
+
+    # dummy_function
+    def dummyFunction(x):
+        pass
+
+    cv.namedWindow(windowName)
+
+    # indidividual trackbars with upper/ lower HSV values in the
+    # "Trackbars" window.
+    cv.createTrackbar(LH,windowName,steps,maxVal,dummyFunction)
+    cv.createTrackbar(LS,windowName,steps,maxVal,dummyFunction)
+    cv.createTrackbar(LV,windowName,steps,maxVal,dummyFunction)
+    cv.createTrackbar(UH,windowName,steps,maxVal,dummyFunction)
+    cv.createTrackbar(US,windowName,steps,maxVal,dummyFunction)
+    cv.createTrackbar(UV,windowName,steps,maxVal,dummyFunction)
+
+
+# Callback function trackbars with masking
+def maskTrackbarsCallback(screenshot ,windowName, LH, LS, LV, UH, US, UV):
+
+    # BGR -> Hue Saturation Value
+    hsv = cv.cvtColor(screenshot, cv.COLOR_BGR2HSV)
+
+
+    # connecting the input of the trackbars to the
+    # to the inside the main loop
+    l_h = cv.getTrackbarPos(LH,windowName)
+    l_s = cv.getTrackbarPos(LS,windowName)
+    l_v = cv.getTrackbarPos(LV,windowName)
+    u_h = cv.getTrackbarPos(UH,windowName)
+    u_s = cv.getTrackbarPos(US,windowName)
+    u_v = cv.getTrackbarPos(UV,windowName)
+
+    # taking the Track bar output to the lower variable
+    lower = np.array([l_h,l_s,l_v])
+    # values we want to detect
+    upper = np.array([u_h,u_s,u_v])
+    # upper and lower are the bounds any value between
+    # those rnges will be detected as white
+
+    mask = cv.inRange(hsv, lower, upper)
+
+    # apply the mask to the frame
+    result = cv.bitwise_and(screenshot, screenshot, mask=mask)
+
+    return result, mask, hsv
+
+
+
--- main.py
+++ main.py
@@ -2,11 +2,8 @@ import cv2 as cv
 import numpy as np
 import time
 from window_capture import WindowCapture
-from template_matching import thresholding, mergeMultiplePositives
-
-
-
-
+from template_matching import *
+from hsv_thresholding import *
 
 
 # define window/ app to capture
@@ -14,62 +11,15 @@ windowCapture = WindowCapture('librewolf')
 windowCapture.start()
 run = True
 
-# load needle image
+
+# load images
 needle_image = cv.imread('./screenshots_diepio/farming/farm_cube2.png', cv.IMREAD_UNCHANGED)
 image = cv.imread('./screenshots_diepio/gameplay_screenshots/Oct30_224501.png', cv.IMREAD_UNCHANGED)
 
 
-# create trackbars
-def TrackbarsHSV(windowName, LH, LS, LV, UH, US, UV, steps, maxVal):
-
-    # dummy_function
-    def dummyFunction(x):
-        pass
-
-    cv.namedWindow(windowName)
-
-    # indidividual trackbars with upper/ lower HSV values in the
-    # "Trackbars" window.
-    cv.createTrackbar(LH,windowName,steps,maxVal,dummyFunction)
-    cv.createTrackbar(LS,windowName,steps,maxVal,dummyFunction)
-    cv.createTrackbar(LV,windowName,steps,maxVal,dummyFunction)
-    cv.createTrackbar(UH,windowName,steps,maxVal,dummyFunction)
-    cv.createTrackbar(US,windowName,steps,maxVal,dummyFunction)
-    cv.createTrackbar(UV,windowName,steps,maxVal,dummyFunction)
-
-
-# Callback function trackbars with masking
-def maskTrackbarsCallback(screenshot ,windowName, LH, LS, LV, UH, US, UV):
-
-    # BGR -> Hue Saturation Value
-    hsv = cv.cvtColor(screenshot, cv.COLOR_BGR2HSV)
-
-
-    # connecting the input of the trackbars to the
-    # to the inside the main loop
-    l_h = cv.getTrackbarPos(LH,windowName)
-    l_s = cv.getTrackbarPos(LS,windowName)
-    l_v = cv.getTrackbarPos(LV,windowName)
-    u_h = cv.getTrackbarPos(UH,windowName)
-    u_s = cv.getTrackbarPos(US,windowName)
-    u_v = cv.getTrackbarPos(UV,windowName)
-
-    # taking the Track bar output to the lower variable
-    lower = np.array([l_h,l_s,l_v])
-    # values we want to detect
-    upper = np.array([u_h,u_s,u_v])
-    # upper and lower are the bounds any value between
-    # those rnges will be detected as white
-
-    mask = cv.inRange(hsv, lower, upper)
-
-    # apply the mask to the frame
-    result = cv.bitwise_and(screenshot, screenshot, mask=mask)
-
-    return result, mask, hsv
-
 # create HSV trackbars for masking
 TrackbarsHSV("Trackbars", "LH","LS","LV","UH","US","UV", 0, 255)
+
 
 # FPS count start
 fps_start = time.time()
@@ -81,13 +31,16 @@ while run:
         #image = windowCapture.screenshot
 
         # image variable needs conversion, otherwise cv.rectangle function throws err.
-        # NOTE: need workaround and color are off.
+        # NOTE: need workaround and colors are off.
         # HACK: this works for now
         screenshot = cv.cvtColor(image, cv.COLOR_BGR2RGB)
         #screenshot = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 
+
+        # NOTE: left for testing purposes
         #positives = thresholding(screenshot, needle_image, 0.35)
         #mergeMultiplePositives(positives, screenshot, needle_image, eps=0.02)
+
 
         # resize the screenshot
         screenshot = cv.resize(screenshot, (900,600))
