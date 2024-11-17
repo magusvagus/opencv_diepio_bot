df08b5a5c0dc97434a5cf3b32ac8d86d7cb75653
magusvagus <magusvagus@proton.me>
2024-11-17 21:37:17 +0100

    corrected path in 'config.yaml', trained AI with custom dataset

--- yolo/main.py
+++ yolo/main.py
@@ -1,20 +1,22 @@
 from ultralytics import YOLO
 
-# Create a new YOLO model from scratch
-model = YOLO("yolo11n.yaml")
+# Create a new YOLO model from scratch (suing yolo 8 nano)
+model = YOLO("yolov8n.yaml")
 
 # Load a pretrained YOLO model (recommended for training)
-model = YOLO("yolo11n.pt")
+#model = YOLO("yolo11n.pt")
 
-# Train the model using the 'coco8.yaml' dataset for 3 epochs
-results = model.train(data="coco8.yaml", epochs=3)
+# Train the model using the 'config.yaml' dataset for 100 epochs
+# epochs = rounds of training
+# imgsz = resize image to set size
+results = model.train(data="config.yaml", epochs=100, imgsz=640)
 
 # Evaluate the model's performance on the validation set
-results = model.val()
+#results = model.val()
 
 # Perform object detection on an image using the model
-results = model("https://ultralytics.com/images/bus.jpg")
+#results = model("https://ultralytics.com/images/bus.jpg")
 
 # Export the model to ONNX format
-success = model.export(format="onnx")
+#success = model.export(format="onnx")
 
