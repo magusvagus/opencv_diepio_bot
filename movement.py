7d65d67d2215f8ad59d28cbd4db1a9ab6fe31320
magusvagus <magusvagus@proton.me>
2024-12-03 18:33:35 +0100

    added comments and code description in movement.py

--- movement.py
+++ movement.py
@@ -1,36 +1,27 @@
-import Xlib
-import Xlib.XK
-import Xlib.display
-from Xlib import X
-from Xlib.ext.xtest import fake_input
 from pynput.keyboard import Key, Controller
 
 # move towards target
+
 # NOTE: HORRIBLE protoype/ proof of concept, needs to be further improved.
 
+# NOTE: Xlib library has been scraped, and replaced with pynput due to more consistant/ predictable results. Future versions will need to go back to Xlib preferbly XCB
 
 
-# WARN: still to be tested!!!
 
-# move into oppsite direction to stop movement
+# WARN: functions still to be tested!!!
+
+# push and release key X times
 def keyPush(key, repeat=1):
 
-    keyboard = Controller()
+    for i in range(repeat):
+        keyboard = Controller()
 
-    keyboard.press(key)
-    keyboard.release(key)
-
+        keyboard.press(key)
+        keyboard.release(key)
 
 
 
 def movement(target ,player):
-
-    # keyboard key push and release example code
-
-    # keysym = Xlib.XK.string_to_keysym('Down')
-    # keycode = display.keysym_to_keycode(keysym)
-    # Xlib.ext.xtest.fake_input(window, Xlib.X.KeyPress, keycode)
-    # Xlib.ext.xtest.fake_input(window, Xlib.X.KeyRelease, keycode)
 
     # FIX: this seems to prever left right movement, probably due to
     # fact that there is no buffer zone in wich the bot can stop. Its
@@ -45,22 +36,28 @@ def movement(target ,player):
     # if target X axis is SMALLER player X axis move towards it
     if target[0] <= (player[0] - distance):
         keyboard = Controller()
+
+        # reset previous pushed keys
         keyboard.release(Key.down)
         keyboard.release(Key.up)
         keyboard.release(Key.left)
         keyboard.release(Key.right)
 
+        # push and realese key
         keyboard.press(Key.left)
         keyboard.press(Key.left)
 
     # if target Y axis is SMALLER player Y axis move towards it
     elif target[1] <= (player[1] - distance):
         keyboard = Controller()
+
+        # reset previous pushed keys
         keyboard.release(Key.down)
         keyboard.release(Key.up)
         keyboard.release(Key.left)
         keyboard.release(Key.right)
 
+        # push and realese key
         keyboard.press(Key.up)
         keyboard.press(Key.up)
 
@@ -68,11 +65,14 @@ def movement(target ,player):
     # if target X axis is BIGGER player X axis move towards it
     elif target[0] >= (player[0] + distance):
         keyboard = Controller()
+
+        # reset previous pushed keys
         keyboard.release(Key.down)
         keyboard.release(Key.up)
         keyboard.release(Key.left)
         keyboard.release(Key.right)
 
+        # push and realese key
         keyboard.press(Key.right)
         keyboard.press(Key.right)
 
@@ -81,17 +81,18 @@ def movement(target ,player):
     elif target[1] >= (player[1] + distance):
         keyboard = Controller()
 
+        # reset previous pushed keys
         keyboard.release(Key.down)
         keyboard.release(Key.up)
         keyboard.release(Key.left)
         keyboard.release(Key.right)
 
+        # push and realese key
         keyboard.press(Key.down)
         keyboard.press(Key.down)
 
 
     else:
-
         keyboard = Controller()
         # reset all previous pushed keys
         keyboard.release(Key.down)
@@ -99,7 +100,8 @@ def movement(target ,player):
         keyboard.release(Key.left)
         keyboard.release(Key.right)
 
-    # if this is added the tank move very slow and steady
+    # if this is added the tank move very very slow this might be
+    # useful
     # keyboard = Controller()
     # keyboard.release(Key.down)
     # keyboard.release(Key.up)
