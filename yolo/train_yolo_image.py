426079f3d26d9373b1f32aa40f9e8f06fb86fecd
magusvagus <magusvagus@proton.me>
2024-12-11 17:24:48 +0100

    added updated YOLO model yolo11n.pt

--- yolo/train_yolo_image.py
+++ yolo/train_yolo_image.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Create a new YOLO model from scratch (suing yolo 8 nano)
-model = YOLO("yolov8n.yaml")
+model = YOLO("yolo11n.pt")
 
 # Load a pretrained YOLO model (recommended for training)
 #model = YOLO("yolo11n.pt")
@@ -9,7 +9,7 @@ model = YOLO("yolov8n.yaml")
 # Train the model using the 'config.yaml' dataset for 100 epochs
 # epochs = rounds of training
 # imgsz = resize image to set size
-results = model.train(data="config.yaml", epochs=1000, imgsz=640, name='yolo_v8n_diep2k')
+results = model.train(data="config.yaml", epochs=1000, imgsz=640, name='yolo_v8n_diep11n')
 
 # Evaluate the model's performance on the validation set
 #results = model.val()
