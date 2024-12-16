88d1a02f7f655ac0dfd5ce9182438bdccbd4b14f
magusvagus <magusvagus@proton.me>
2024-12-16 16:41:41 +0100

    modified levels.py
    fixed issue with wrong brackets in the file, also color was pushed out to each parent dictionary

--- levels.py
+++ levels.py
@@ -1,364 +1,279 @@
 # this file has all the coordinates and color data of the leveling UI
 # NOTE: the current coordinates and RGB values in this file are just placeholders until being updated.
 
-def skillsTree(image):
+def skillTree(image):
 
     #main dictionary
     skills = {
         # skill bar name
         'health_regen' : {
-                'level1' : {
-                    'level':1,
-                    'coordinates':image[10,10],
-                    'color':(11,11,11),
-                },
+            'color':(11,11,11),
 
-                'level2' : {
-                    'level':2,
-                    'coordinates':image[10,10],
-                    'color':(22,22,22),
-                },
-
-                'level3' : {
-                    'level':3,
-                    'coordinates':image[10,10],
-                    'color':(33,33,33),
-                },
-
-                'level4' : {
-                    'level':4,
-                    'coordinates':image[10,10],
-                    'color':(44,44,44),
-                },
-
-                'level5' : {
-                    'level':5,
-                    'coordinates':image[10,10],
-                    'color':(55,55,55),
-                },
-
-                'level6' : { 
-                    'level':6,
-                    'coordinates':image[10,10],
-                    'color':(66,66,66),
-                },
-
-                'level7' : { 
-                    'level':7,
-                    'coordinates':image[10,10],
-                    'color':(77,77,77),
-                },
+            'level1' : {
+                'level':1,
+                'coordinates':image[10,10],
             },
-
-            'max_health' : {
-                'level1' : {
-                    'level':1,
-                    'coordinates':image[10,10],
-                    'color':(11,11,11),
-                },
-
-                'level2' : {
-                    'level':2,
-                    'coordinates':image[10,10],
-                    'color':(22,22,22),
-                },
-
-                'level3' : {
-                    'level':3,
-                    'coordinates':image[10,10],
-                    'color':(33,33,33),
-                },
-
-                'level4' : {
-                    'level':4,
-                    'coordinates':image[10,10],
-                    'color':(44,44,44),
-                },
-
-                'level5' : {
-                    'level':5,
-                    'coordinates':image[10,10],
-                    'color':(55,55,55),
-                },
-
-                'level6' : { 
-                    'level':6,
-                    'coordinates':image[10,10],
-                    'color':(66,66,66),
-                },
-
-                'level7' : { 
-                    'level':7,
-                    'coordinates':image[10,10],
-                    'color':(77,77,77),
-                },
-
-            'body_damage' : {
-                'level1' : {
-                    'level':1,
-                    'coordinates':image[10,10],
-                    'color':(11,11,11),
-                },
-
-                'level2' : {
-                    'level':2,
-                    'coordinates':image[10,10],
-                    'color':(22,22,22),
-                },
-
-                'level3' : {
-                    'level':3,
-                    'coordinates':image[10,10],
-                    'color':(33,33,33),
-                },
-
-                'level4' : {
-                    'level':4,
-                    'coordinates':image[10,10],
-                    'color':(44,44,44),
-                },
-
-                'level5' : {
-                    'level':5,
-                    'coordinates':image[10,10],
-                    'color':(55,55,55),
-                },
-
-                'level6' : { 
-                    'level':6,
-                    'coordinates':image[10,10],
-                    'color':(66,66,66),
-                },
-
-                'level7' : { 
-                    'level':7,
-                    'coordinates':image[10,10],
-                    'color':(77,77,77),
-                },
+            'level2' : {
+                'level':2,
+                'coordinates':image[10,10],
             },
-
-            'bullet_speed' : {
-                'level1' : {
-                    'level':1,
-                    'coordinates':image[10,10],
-                    'color':(11,11,11),
-                },
-
-                'level2' : {
-                    'level':2,
-                    'coordinates':image[10,10],
-                    'color':(22,22,22),
-                },
-
-                'level3' : {
-                    'level':3,
-                    'coordinates':image[10,10],
-                    'color':(33,33,33),
-                },
-
-                'level4' : {
-                    'level':4,
-                    'coordinates':image[10,10],
-                    'color':(44,44,44),
-                },
-
-                'level5' : {
-                    'level':5,
-                    'coordinates':image[10,10],
-                    'color':(55,55,55),
-                },
-
-                'level6' : { 
-                    'level':6,
-                    'coordinates':image[10,10],
-                    'color':(66,66,66),
-                },
-
-                'level7' : { 
-                    'level':7,
-                    'coordinates':image[10,10],
-                    'color':(77,77,77),
-                },
+            'level3' : {
+                'level':3,
+                'coordinates':image[10,10],
             },
-
-            'bullet_penetration' : {
-                'level1' : {
-                    'level':1,
-                    'coordinates':image[10,10],
-                    'color':(11,11,11),
-                },
-
-                'level2' : {
-                    'level':2,
-                    'coordinates':image[10,10],
-                    'color':(22,22,22),
-                },
-
-                'level3' : {
-                    'level':3,
-                    'coordinates':image[10,10],
-                    'color':(33,33,33),
-                },
-
-                'level4' : {
-                    'level':4,
-                    'coordinates':image[10,10],
-                    'color':(44,44,44),
-                },
-
-                'level5' : {
-                    'level':5,
-                    'coordinates':image[10,10],
-                    'color':(55,55,55),
-                },
-
-                'level6' : { 
-                    'level':6,
-                    'coordinates':image[10,10],
-                    'color':(66,66,66),
-                },
-
-                'level7' : { 
-                    'level':7,
-                    'coordinates':image[10,10],
-                    'color':(77,77,77),
-                },
+            'level4' : {
+                'level':4,
+                'coordinates':image[10,10],
             },
-
-            'bullet_damage' : {
-                'level1' : {
-                    'level':1,
-                    'coordinates':image[10,10],
-                    'color':(11,11,11),
-                },
-
-                'level2' : {
-                    'level':2,
-                    'coordinates':image[10,10],
-                    'color':(22,22,22),
-                },
-
-                'level3' : {
-                    'level':3,
-                    'coordinates':image[10,10],
-                    'color':(33,33,33),
-                },
-
-                'level4' : {
-                    'level':4,
-                    'coordinates':image[10,10],
-                    'color':(44,44,44),
-                },
-
-                'level5' : {
-                    'level':5,
-                    'coordinates':image[10,10],
-                    'color':(55,55,55),
-                },
-
-                'level6' : { 
-                    'level':6,
-                    'coordinates':image[10,10],
-                    'color':(66,66,66),
-                },
-
-                'level7' : { 
-                    'level':7,
-                    'coordinates':image[10,10],
-                    'color':(77,77,77),
-                },
+            'level5' : {
+                'level':5,
+                'coordinates':image[10,10],
             },
-
-            'relaod' : {
-                'level1' : {
-                    'level':1,
-                    'coordinates':image[10,10],
-                    'color':(11,11,11),
-                },
-
-                'level2' : {
-                    'level':2,
-                    'coordinates':image[10,10],
-                    'color':(22,22,22),
-                },
-
-                'level3' : {
-                    'level':3,
-                    'coordinates':image[10,10],
-                    'color':(33,33,33),
-                },
-
-                'level4' : {
-                    'level':4,
-                    'coordinates':image[10,10],
-                    'color':(44,44,44),
-                },
-
-                'level5' : {
-                    'level':5,
-                    'coordinates':image[10,10],
-                    'color':(55,55,55),
-                },
-
-                'level6' : { 
-                    'level':6,
-                    'coordinates':image[10,10],
-                    'color':(66,66,66),
-                },
-
-                'level7' : { 
-                    'level':7,
-                    'coordinates':image[10,10],
-                    'color':(77,77,77),
-                },
+            'level6' : { 
+                'level':6,
+                'coordinates':image[10,10],
             },
-
-            'movement_speed' : {
-                'level1' : {
-                    'level':1,
-                    'coordinates':image[10,10],
-                    'color':(11,11,11),
-                },
-
-                'level2' : {
-                    'level':2,
-                    'coordinates':image[10,10],
-                    'color':(22,22,22),
-                },
-
-                'level3' : {
-                    'level':3,
-                    'coordinates':image[10,10],
-                    'color':(33,33,33),
-                },
-
-                'level4' : {
-                    'level':4,
-                    'coordinates':image[10,10],
-                    'color':(44,44,44),
-                },
-
-                'level5' : {
-                    'level':5,
-                    'coordinates':image[10,10],
-                    'color':(55,55,55),
-                },
-
-                'level6' : { 
-                    'level':6,
-                    'coordinates':image[10,10],
-                    'color':(66,66,66),
-                },
-
-                'level7' : { 
-                    'level':7,
-                    'coordinates':image[10,10],
-                    'color':(77,77,77),
-                },
+            'level7' : { 
+                'level':7,
+                'coordinates':image[10,10],
             },
-
         },
 
+        'max_health' : {
+            'color':(77,77,77),
+
+            'level1' : {
+                'level':1,
+                'coordinates':image[10,10],
+            },
+            'level2' : {
+                'level':2,
+                'coordinates':image[10,10],
+            },
+            'level3' : {
+                'level':3,
+                'coordinates':image[10,10],
+            },
+            'level4' : {
+                'level':4,
+                'coordinates':image[10,10],
+            },
+            'level5' : {
+                'level':5,
+                'coordinates':image[10,10],
+            },
+            'level6' : { 
+                'level':6,
+                'coordinates':image[10,10],
+            },
+            'level7' : { 
+                'level':7,
+                'coordinates':image[10,10],
+            },
+        },
+
+        'body_damage' : {
+            'color':(77,77,77),
+
+            'level1' : {
+                'level':1,
+                'coordinates':image[10,10],
+            },
+            'level2' : {
+                'level':2,
+                'coordinates':image[10,10],
+            },
+            'level3' : {
+                'level':3,
+                'coordinates':image[10,10],
+            },
+            'level4' : {
+                'level':4,
+                'coordinates':image[10,10],
+            },
+            'level5' : {
+                'level':5,
+                'coordinates':image[10,10],
+            },
+            'level6' : { 
+                'level':6,
+                'coordinates':image[10,10],
+            },
+            'level7' : { 
+                'level':7,
+                'coordinates':image[10,10],
+            },
+        },
+
+        'bullet_speed' : {
+            'color':(77,77,77),
+
+            'level1' : {
+                'level':1,
+                'coordinates':image[10,10],
+            },
+            'level2' : {
+                'level':2,
+                'coordinates':image[10,10],
+            },
+            'level3' : {
+                'level':3,
+                'coordinates':image[10,10],
+            },
+            'level4' : {
+                'level':4,
+                'coordinates':image[10,10],
+            },
+            'level5' : {
+                'level':5,
+                'coordinates':image[10,10],
+            },
+            'level6' : { 
+                'level':6,
+                'coordinates':image[10,10],
+            },
+            'level7' : { 
+                'level':7,
+                'coordinates':image[10,10],
+            },
+        },
+
+        'bullet_penetration' : {
+            'color':(77,77,77),
+
+            'level1' : {
+                'level':1,
+                'coordinates':image[10,10],
+            },
+            'level2' : {
+                'level':2,
+                'coordinates':image[10,10],
+            },
+            'level3' : {
+                'level':3,
+                'coordinates':image[10,10],
+            },
+            'level4' : {
+                'level':4,
+                'coordinates':image[10,10],
+            },
+            'level5' : {
+                'level':5,
+                'coordinates':image[10,10],
+            },
+            'level6' : { 
+                'level':6,
+                'coordinates':image[10,10],
+            },
+            'level7' : { 
+                'level':7,
+                'coordinates':image[10,10],
+            },
+        },
+
+        'bullet_damage' : {
+            'color':(77,77,77),
+            'level1' : {
+                'level':1,
+                'coordinates':image[10,10],
+            },
+            'level2' : {
+                'level':2,
+                'coordinates':image[10,10],
+            },
+            'level3' : {
+                'level':3,
+                'coordinates':image[10,10],
+            },
+            'level4' : {
+                'level':4,
+                'coordinates':image[10,10],
+            },
+            'level5' : {
+                'level':5,
+                'coordinates':image[10,10],
+            },
+            'level6' : { 
+                'level':6,
+                'coordinates':image[10,10],
+            },
+            'level7' : { 
+                'level':7,
+                'coordinates':image[10,10],
+            },
+        },
+
+        'relaod' : {
+            'color':(77,77,77),
+
+            'level1' : {
+                'level':1,
+                'coordinates':image[10,10],
+            },
+            'level2' : {
+                'level':2,
+                'coordinates':image[10,10],
+            },
+            'level3' : {
+                'level':3,
+                'coordinates':image[10,10],
+            },
+            'level4' : {
+                'level':4,
+                'coordinates':image[10,10],
+            },
+            'level5' : {
+                'level':5,
+                'coordinates':image[10,10],
+            },
+            'level6' : { 
+                'level':6,
+                'coordinates':image[10,10],
+            },
+            'level7' : { 
+                'level':7,
+                'coordinates':image[10,10],
+            },
+        },
+
+        'movement_speed' : {
+            'color':(77,77,77),
+
+            'level1' : {
+                'level':1,
+                'coordinates':image[10,10],
+            },
+
+            'level2' : {
+                'level':2,
+                'coordinates':image[10,10],
+            },
+
+            'level3' : {
+                'level':3,
+                'coordinates':image[10,10],
+            },
+
+            'level4' : {
+                'level':4,
+                'coordinates':image[10,10],
+            },
+
+            'level5' : {
+                'level':5,
+                'coordinates':image[10,10],
+            },
+
+            'level6' : { 
+                'level':6,
+                'coordinates':image[10,10],
+            },
+
+            'level7' : { 
+                'level':7,
+                'coordinates':image[10,10],
+            },
+        },
     }
 
     return skills
