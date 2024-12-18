828649dc791f3c863729979b36d76401665f3660
magusvagus <magusvagus@proton.me>
2024-12-18 22:21:53 +0100

    re-added coordinates

--- levels.py
+++ levels.py
@@ -6,6 +6,7 @@ def skillsTree(image):
     #main dictionary
     skills = {
         # skill bar name
+            # NOTE: coordinates needed
             'health_regen' : {
                 'level1' : {
                     'level':1,
@@ -42,6 +43,7 @@ def skillsTree(image):
                     'color':(77,77,77),
                 },
             },
+            # NOTE: coordinates needed
             'max_health' : {
                 'level1' : {
                     'level':1,
@@ -79,6 +81,7 @@ def skillsTree(image):
                     'color':(77,77,77),
                 },
             },
+            # NOTE: coordinates needed
             'body_damage' : {
                 'level1' : {
                     'level':1,
@@ -116,6 +119,7 @@ def skillsTree(image):
                     'color':(77,77,77),
                 },
             },
+            # NOTE: coordinates needed
             'bullet_speed' : {
                 'level1' : {
                     'level':1,
@@ -156,114 +160,115 @@ def skillsTree(image):
             'bullet_penetration' : {
                 'level1' : {
                     'level':1,
-                    'coordinates':image[10,10],
+                    'coordinates':image[46,978],
                     'color':(11,11,11),
                 },
                 'level2' : {
                     'level':2,
-                    'coordinates':image[10,10],
+                    'coordinates':image[73,978],
                     'color':(22,22,22),
                 },
                 'level3' : {
                     'level':3,
-                    'coordinates':image[10,10],
+                    'coordinates':image[95,978],
                     'color':(33,33,33),
                 },
                 'level4' : {
                     'level':4,
-                    'coordinates':image[10,10],
+                    'coordinates':image[118,979],
                     'color':(44,44,44),
                 },
                 'level5' : {
                     'level':5,
-                    'coordinates':image[10,10],
+                    'coordinates':image[140,979],
                     'color':(55,55,55),
                 },
                 'level6' : { 
                     'level':6,
-                    'coordinates':image[10,10],
+                    'coordinates':image[165,979],
                     'color':(66,66,66),
                 },
                 'level7' : { 
                     'level':7,
-                    'coordinates':image[10,10],
+                    'coordinates':image[186,979],
                     'color':(77,77,77),
                 },
             },
             'bullet_damage' : {
                 'level1' : {
                     'level':1,
-                    'coordinates':image[10,10],
+                    'coordinates':image[45,996],
                     'color':(11,11,11),
                 },
                 'level2' : {
                     'level':2,
-                    'coordinates':image[10,10],
+                    'coordinates':image[71,1002],
                     'color':(22,22,22),
                 },
                 'level3' : {
                     'level':3,
-                    'coordinates':image[10,10],
+                    'coordinates':image[94,1001],
                     'color':(33,33,33),
                 },
                 'level4' : {
                     'level':4,
-                    'coordinates':image[10,10],
+                    'coordinates':image[114,1001],
                     'color':(44,44,44),
                 },
                 'level5' : {
                     'level':5,
-                    'coordinates':image[10,10],
+                    'coordinates':image[141,1001],
                     'color':(55,55,55),
                 },
                 'level6' : { 
                     'level':6,
-                    'coordinates':image[10,10],
+                    'coordinates':image[165,1001],
                     'color':(66,66,66),
                 },
                 'level7' : { 
                     'level':7,
-                    'coordinates':image[10,10],
+                    'coordinates':image[188,1002],
                     'color':(77,77,77),
                 },
             },
             'relaod' : {
                 'level1' : {
                     'level':1,
-                    'coordinates':image[10,10],
+                    'coordinates':image[44,1025],
                     'color':(11,11,11),
                 },
                 'level2' : {
                     'level':2,
-                    'coordinates':image[10,10],
+                    'coordinates':image[70,1025],
                     'color':(22,22,22),
                 },
                 'level3' : {
                     'level':3,
-                    'coordinates':image[10,10],
+                    'coordinates':image[94,1025],
                     'color':(33,33,33),
                 },
                 'level4' : {
                     'level':4,
-                    'coordinates':image[10,10],
+                    'coordinates':image[117,1025],
                     'color':(44,44,44),
                 },
                 'level5' : {
                     'level':5,
-                    'coordinates':image[10,10],
+                    'coordinates':image[141,1025],
                     'color':(55,55,55),
                 },
                 'level6' : { 
                     'level':6,
-                    'coordinates':image[10,10],
+                    'coordinates':image[163,1025],
                     'color':(66,66,66),
                 },
                 'level7' : { 
                     'level':7,
-                    'coordinates':image[10,10],
+                    'coordinates':image[189,1025],
                     'color':(77,77,77),
                 },
             },
+            # NOTE: coordinates needed
             'movement_speed' : {
                 'level1' : {
                     'level':1,
